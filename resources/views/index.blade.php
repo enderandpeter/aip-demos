9801c760a822a7ceb300c6baa7fc2296deb4f067
Spencer Williams <spencer@aninternetpresence.net>
2016-06-25 21:56:51 -0600

    Replace list items with definition list.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,12 +18,6 @@
 	<div class="page-header">
 		<h2>Demos</h2>
 	</div>
-	<ul>
-		<li><a href="search-my-backyard">Search my Backyard!</a> — <a href="https://www.udacity.com" target="_blank">Udacity</a> — <a href="https://www.udacity.com/course/javascript-design-patterns--ud989" target="_blank">JavaScript Design Patterns</a>  final project</li>
-		<li><a href="frogger">Effective JavaScript: Frogger</a> — <a href="https://www.udacity.com" target="_blank">Udacity</a> — <a href="https://www.udacity.com/course/object-oriented-javascript--ud015" target="_blank">Object-Oriented JavaScript</a>  final project</li>
-		<li><a href="{{ env('BRL_URL', 'http://brl.aninternetpresence.net') }}" target="_blank">Booj Reading List</a> — <a href="http://booj.com/" target="_blank">Booj</a> — <a href="https://github.com/enderandpeter/boojbooks" target="_blank">Application Code Test</a></li>
-		<li><a href="event-planner">Event Planner</a> — <a href="https://www.udacity.com" target="_blank">Udacity</a> — <a href="https://www.udacity.com/course/building-high-conversion-web-forms--ud890" target="_blank">Building High Conversion Web Forms</a>  final project</li>
-	</ul>
 	<dl class="dl-horizontal">
 		<dt><a target="_blank" href="https://www.udacity.com">Udacity</a></dt>
 			<dd><a href="search-my-backyard">Search my Backyard!</a> &mdash; <a target="_blank" href="https://www.udacity.com/course/javascript-design-patterns--ud989">JavaScript Design Patterns</a>  final project</dd>
