f97f346a54e7909b0695de0ef229379f686b348d
Spencer Williams <spencer@aninternetpresence.net>
2016-06-27 23:19:38 -0600

    Add tests to confirm correct date displays when entered

--- tests/EventCalendarTest.php
+++ tests/EventCalendarTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Carbon\Carbon;
+use Faker\Factory as Faker;
 
 class EventCalendarTest extends TestCase
 {
@@ -17,15 +18,44 @@ class EventCalendarTest extends TestCase
     	
     	$date = Carbon::now();
     	
-    	$monthFormat = 'F Y';
+    	$dateHeaderFormat = 'F Y';
     	
     	$headerID = 'calendarHeading';
-    	$headerText = $date->format($monthFormat);
+    	$headerText = $date->format($dateHeaderFormat);
     	
         $this->visit('/event-planner')
              ->seeInElement('#calendarHeading', $headerText) // Header text should be present
         	 ->seeIsSelected('#month', $date->format('n')) // Header should have current date
         	 ->seeInField('#year', $date->format('Y'))
         	 ->seeInElement('#today', $date->format('j')); // The current day should be in the appropriate element
+    }
+    
+    /**
+     * Test to make sure the date entered into the form is loaded
+     */
+    public function testGoToDate(){
+    	$faker = Faker::create();
+    	
+    	$date = Carbon::instance($faker->dateTime());
+    	$dateHeaderFormat = 'F Y';
+    	
+    	$this->visit('/event-planner')
+    		  ->select($date->month, '#month')
+    		 ->type($date->year, '#year')
+    		 ->press('Go')
+    		 ->see($date->format($dateHeaderFormat));
+    }
+    
+    /**
+     * Test to make sure the current date will load when the button is clicked
+     */
+    public function testGoToCurrentDate(){
+    	$date = Carbon::now();
+    	 
+    	$dateHeaderFormat = 'F Y';
+    	 
+    	$this->visit('/event-planner')
+    	->press('Today')
+    	->see($date->format($dateHeaderFormat));
     }
 }
