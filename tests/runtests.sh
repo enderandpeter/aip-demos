91daadf1841b8c474392f2ec48aa1e84fd82a788
Spencer Williams IV <spencer@aninternetpresence.net>
2017-05-24 22:14:04 -0600

    Use forward slashes in .sh script

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -5,4 +5,4 @@ php artisan dusk --group=create-today && \
 php artisan dusk --group=create-datefields && \
 php artisan dusk --group=create-success && \
 php artisan dusk --exclude-group=loginas && \
-vendor\bin\phpunit
+vendor/bin/phpunit
