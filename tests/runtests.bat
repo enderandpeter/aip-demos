1e64ac4a9b6aab279bd966af460d8b6d68a1a541
Spencer Williams <spencer@aninternetpresence.net>
2017-06-01 16:30:23 -0600

    Make sure tests scripts refer to all the right tests

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -6,5 +6,7 @@ php artisan dusk --group=create-startdatefield & ^
 php artisan dusk --group=create-enddatefield & ^
 php artisan dusk --group=create-success & ^
 php artisan dusk --group=edit-calendarevent & ^
+php artisan dusk --group=delete-success & ^
+php artisan dusk --group=delete-cancel & ^
 php artisan dusk --exclude-group=loginas & ^
 vendor\bin\phpunit
