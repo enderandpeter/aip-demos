83579c6590b0269b8e2dac82a135dbac2d5bfebf
Spencer Williams <spencer@aninternetpresence.net>
2017-11-01 10:51:14 -0600

    Update tests to work with Docker and TravisCI
      * When Chrome runs in headless mode, the Dusk tests can't see anything
        on the page. They work fine when Chrome is not in headless mode
        and Xvfb is running in the background.

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Laravel\Dusk\TestCase as BaseTestCase;
+use Exception;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
@@ -10,6 +11,47 @@ use Facebook\WebDriver\Remote\DesiredCapabilities;
 abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
+    
+    public static function setUpBeforeClass()
+    {
+        if(self::isTextEnvironment()){
+            exec('Xvfb -ac :0 -screen 0 1280x1024x16 &', $output, $returnValue);
+            $returnValue = intval($returnValue);
+            
+            if($returnValue !== 0){
+                throw new Exception('Could not start Xvfb');
+            }
+        }  
+    }
+    
+    public static function tearDownAfterClass()
+    {
+        if(self::isTextEnvironment()){
+            $pids_Xvfb = exec('pidof Xvfb');
+            if(!empty($pids_Xvfb)){
+                exec("kill -9 $pids_Xvfb");
+            }
+        }
+    }
+    
+    /**
+     * @return boolean
+     */
+    private static function isTextEnvironment()
+    {
+        if(PHP_OS === 'Linux'){
+            $kernelName = exec('uname -r');
+            $osInfo = exec('lsb_release -r');
+        
+            if(str_contains($kernelName, 'moby') /* Docker */ ||
+                str_contains($osInfo, 'trusty') /* TravisCI */){
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
     
     /**
      * Prepare for Dusk test execution.
@@ -29,11 +71,14 @@ abstract class DuskTestCase extends BaseTestCase
      */
     protected function driver()
     {
-        $options = (new ChromeOptions)->addArguments([
-            '--disable-gpu',
-            '--headless',
-            '--no-sandbox'
-        ]);
+        $defaultSettings = [];
+        $arguments = [];
+        
+        if(self::isTextEnvironment()){
+            $arguments = array_merge($defaultSettings, ['--disable-gpu', '--no-sandbox']);
+        }
+        
+        $options = (new ChromeOptions)->addArguments($arguments);
         
         return RemoteWebDriver::create(
             'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
