2a37f4fa513b85f8896f3032384850fd4b3b2df9
Spencer Williams <spencer@aninternetpresence.net>
2017-11-08 10:24:24 -0700

    Add Bootstrap 4 Beta and Google Material icons
      * Restore Search My Backyard icons with Google Material Icons
      * use btn-light instead of removed btn-default

--- resources/views/scripts/google-maps.blade.php
+++ resources/views/scripts/google-maps.blade.php
@@ -1,3 +1,3 @@
 @prepend('scripts')
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAlGK97uekQTDMR4h7Wr5lLtENUgpOD7eo&v=3" async defer></script>	
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAlGK97uekQTDMR4h7Wr5lLtENUgpOD7eo&v=3" async defer></script>	
 @endprepend
